b26af5722f02022f6939787ac4b0e5feaef406c8
lilprof01 <adewumime@gmail.com>
2025-05-01 13:59:54 +0100

    testing

--- src/components/main/index.ts
+++ src/components/main/index.ts
@@ -2,3 +2,4 @@ export { default as Hero } from "./Hero";
 export { default as Features } from "./Features";
 export { default as WhyChooseUs } from "./WhyChooseUs";
 export { default as Testimonial } from "./Testimonial";
+export { default as FAQ } from "./FAQ";
